a465051305951a9972f194caf7c393be5b8f9bc1
Abdul Mujeeb <mujeeb.mjcet@gmail.com>
2019-10-26 15:12:50 +0400

    Commented Security Code

--- BackendWebAPI/BackendWebAPI/Controllers/WeatherForecastController.cs
+++ BackendWebAPI/BackendWebAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace BackendWebAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Hatim123", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
